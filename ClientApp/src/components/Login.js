7a62c49abc6f704a55fcff8a60d2497b2efeaeda
Damian <damian1003@onet.pl>
2023-05-25 23:13:22 +0200

    login frontendu

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,4 +1,35 @@
-﻿import "./login.css"
+﻿import axios from "axios"
+
+import "./login.css"
+
+const setAuthToken = token => {
+    if (token) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    }
+    else
+        delete axios.defaults.headers.common["Authorization"];
+}
+
+//TODO: endpoint dla tego axiosa
+const handleSubmit = (login, password) => {
+    const loginPayload = {
+        login: "admin",
+        password: "123"
+    }
+
+    axios.post("/user", loginPayload)
+        .then(response => {
+            const token = response.data.token;
+
+            localStorage.setItem("token", token)
+
+            setAuthToken(token)
+            console.log(token)
+            console.log("DHUIDHAWOUIDAWFD")
+
+            window.location.href = "/"
+        }).catch(console.log("duhiafbae"))
+}
 
 export function Login() {
     return (
@@ -7,12 +38,13 @@ export function Login() {
                 <div className="container page d-flex justify-content-center align-items-center min-vh-100">
                     <div className="row">
                         <div className="login-box">
-                            <h1 className="text-login">Logowanie</h1>
-                            <form>
+                            <h1 className="text-login">SERWIS XYZ</h1>
+                            <h2 className="text-login">LOGOWANIE</h2>
+                            <form onSubmit={handleSubmit}>
                                 <fieldset>
                                     <fieldset className="form-group">
                                         <input
-                                            placeholder="Email"
+                                            placeholder="Login"
                                             className="form-control form-control-lg"
                                             type="text"
                                         />
@@ -32,6 +64,9 @@ export function Login() {
                                     </div>
                                     <div className="forgot-pass d-flex justify-content-center align-items-center">
                                         <a> Zapomniałeś hasła?</a>
+                                    </div>
+                                    <div className="forgot-pass d-flex justify-content-center align-items-center">
+                                        <a> Nie posiadasz konta?</a>
                                     </div>
 
                                     <div className="forgot-pass d-flex justify-content-center align-items-center">
