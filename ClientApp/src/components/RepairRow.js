5a8adbe3241978c6249939a2e894d12132783080
Damian <damian1003@onet.pl>
2023-06-05 20:07:34 +0200

    Dodany repairRow

---
+++ ClientApp/src/components/RepairRow.js
@@ -0,0 +1,21 @@
+﻿
+
+export function RepairRow ({ repair }) {
+
+
+    // TODO: Trzeba status na enuma przerobić
+    return (
+        <tr>
+            <td>{repair.id}</td>
+            <td>{ repair.status }</td>
+            <td>{ repair.client.firstName ?? "-" }</td>
+            <td>{ repair.client.lastName ?? "-" }</td>
+            <td>{ repair.equipment.name ?? "-" }</td>
+            <td>
+                <button className='label-button'>EDYTUJ</button>
+                <button className='label-button'>SZCZEGÓŁY</button>
+                <button className='label-button'>USUŃ</button>
+            </td>
+        </tr>
+    )
+}
