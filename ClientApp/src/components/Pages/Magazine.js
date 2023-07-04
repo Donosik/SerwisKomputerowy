6d795548bc13d341e93f0f0f71a239d712e9bd9c
Damian <damian1003@onet.pl>
2023-07-04 16:10:50 +0200

    poprawiony magazyn

--- ClientApp/src/components/Pages/Magazine.js
+++ ClientApp/src/components/Pages/Magazine.js
@@ -1,7 +1,18 @@
 import {NavMenu} from "../Components/NavMenu";
 import "../Css/Magazine.css"
+import {useState} from "react";
+import {SearchMagazine} from "../Components/SearchMagazine";
+import {AddMagazine} from "../Components/AddMagazine";
 export function Magazine()
 {
+    const [renderBool, setRenderBool] = useState(true)
+
+    function searchHandler() {
+        setRenderBool(true)
+    }
+    function createHandler() {
+        setRenderBool(false)
+    }
     return(
         <>
             <NavMenu/>
