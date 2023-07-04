470e443e93ba828c3d804112c15793428b8cb2f5
kroolremizy <rafal.kciuk.1@gmail.com>
2023-07-04 20:43:12 +0200

    repair table fixes

--- ClientApp/src/components/Pages/Chatbox.js
+++ ClientApp/src/components/Pages/Chatbox.js
@@ -1,7 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Css/Chatbox.css';
 import '../Css/EditUser.css';
 import { NavMenu } from "../Components/NavMenu";
+import axios from 'axios';
+
+const RepairTable = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('/repair')
+            .then(response => {
+                setData(response.data);
+            })
+    }, []);
+
+    return (
+        <>
+            <NavMenu />
+            <p className='services-title'> WIADOMOŚCI </p>
+            <div className="chatbox-container">
+                <div className="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID Naprawy</th>
+                                <th>Imie</th>
+                                <th>Nazwisko</th>
+                                <th>Akcje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map(item => (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.client.firstName}</td>
+                                    <td>{item.client.lastName}</td>
+                                    <td>
+                                       
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
+    }
 
 function Chatbox() {
     const [messages, setMessages] = useState([]);
@@ -23,63 +68,30 @@ function Chatbox() {
         }
     };
 
-    const tableData = [
-        { id: 1, data1: 'Data1', data2: 'Data2', data3: 'Data3' },
-        { id: 2, data1: 'Data4', data2: 'Data5', data3: 'Data6' },
-    ];
-
     const handleRowButtonClick = (rowId) => {
         console.log('Button pressed for ID: ', rowId);
     }
 
     return (
         <>
-            <NavMenu />
-            <p className='services-title'> WIADOMOŚCI </p>
-            <div className="chatbox-container">
-                <div className="table-container">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Header1</th>
-                                <th>Header2</th>
-                                <th>Header3</th>
-                                <th>Akcje</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {tableData.map((row) => (
-                                <tr key={row.id}>
-                                    <td>{row.data1}</td>
-                                    <td>{row.data2}</td>
-                                    <td>{row.data3}</td>
-                                    <td>
-                                        <button class="button-class" onClick={() => handleRowButtonClick(row.id)}>Przycisk</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
             <div className="chatbox">
-            <div className="messages">
-                {messages.map((message) => (
-                    <div key={message.id} className="message">
-                        {message.text}
-                    </div>
-                ))}
+                <div className="messages">
+                    {messages.map((message) => (
+                        <div key={message.id} className="message">
+                            {message.text}
+                        </div>
+                    ))}
+                </div>
+                <div className="input-container">
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        placeholder="Type your message..."
+                    />
+                    <button onClick={handleSendMessage}>Send</button>
+                </div>
             </div>
-            <div className="input-container">
-                <input
-                    type="text"
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    placeholder="Type your message..."
-                />
-                <button onClick={handleSendMessage}>Send</button>
-                </div>
-                </div>
-        </div>
             </>
     );
 }
