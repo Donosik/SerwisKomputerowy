f9472918f38eef2f8a36e357d86d9126e129b95a
Agnieszka <aga.g6664@gmail.com>
2023-07-04 13:01:14 +0200

    walidacja cyfry imie / nazwisko

--- ClientApp/src/components/Pages/Register.js
+++ ClientApp/src/components/Pages/Register.js
@@ -1,57 +1,64 @@
-﻿import axios from "axios"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+﻿import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import "../Css/register.css"
+import "../Css/register.css";
 
-const setAuthToken = token => {
+const setAuthToken = (token) => {
     if (token) {
         axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    }
-    else
+    } else {
         delete axios.defaults.headers.common["Authorization"];
-}
+    }
+};
 
 export function Register() {
-
-    const navigate = useNavigate()
-    const [login, setLogin] = useState('')
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [password, setPassword] = useState('')
-    const [registerFailed, setRegisterFailed] = useState(false)
-    
+    const navigate = useNavigate();
+    const [login, setLogin] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+    const [registerFailed, setRegisterFailed] = useState("");
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
+
+        // Walidacja imienia i nazwiska
+        const nameRegex = /^[a-zA-Z\s]*$/;
+        if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+            setRegisterFailed("Imię i nazwisko nie mogą zawierać liczb");
+            return;
+        }
 
         try {
-            if (login.length >= 3 && password.length >= 3 && firstName.length >= 1 && lastName.length >= 1)
-            {
-                const response = await axios.post('/user/register', { login, password, firstName, lastName })
+            if (
+                login.length >= 3 &&
+                password.length >= 3 &&
+                firstName.length >= 1 &&
+                lastName.length >= 1
+            ) {
+                const response = await axios.post("/user/register", {
+                    login,
+                    password,
+                    firstName,
+                    lastName,
+                });
 
-                navigate("/logowanie")
-                setRegisterFailed('')
+                navigate("/logowanie");
+                setRegisterFailed("");
+            } else if (firstName.length <= 1 || lastName.length <= 1) {
+                setRegisterFailed("Pola imię i nazwisko muszą być wypełnione");
+            } else {
+                setRegisterFailed("Login i hasło muszą mieć minimum 3 znaki");
             }
-            else if (firstName.length <= 1 || lastName.length <= 1)
-            {
-                setRegisterFailed('Pola imię i nazwisko muszą być wypełnione')
-            }
-            else {
-                setRegisterFailed('Login i hasło muszą mieć minimum 3 znaki')
-            }
-
         } catch (error) {
-            setRegisterFailed('Podany login już jest w bazie')
-            console.log(error)
+            setRegisterFailed("Podany login już istnieje w bazie");
+            console.log(error);
         }
-    }
-
-    
+    };
 
     return (
         <>
-
             <div className="auth-page">
                 <div className="navBar">
                     <h1 className="text-welcome">WWW.KOMPUTEROWO.PL</h1>
@@ -60,7 +67,11 @@ export function Register() {
                     <div className="row">
                         <div className="reg-box">
                             <h2 className="text-login">REJESTRACJA</h2>
-                            <h5 className="text-login2">Login musi być unikalny, min 3 znaki <br/> Hasło min 3 znaki</h5>
+                            <h5 className="text-login2">
+                                Login musi być unikalny, min 3 znaki
+                                <br />
+                                Hasło min 3 znaki
+                            </h5>
                             <form>
                                 <fieldset>
                                     <fieldset className="form-group">
@@ -69,7 +80,7 @@ export function Register() {
                                             className="form-control form-control-lg"
                                             type="text"
                                             value={login}
-                                            onChange={e => setLogin(e.target.value)}
+                                            onChange={(e) => setLogin(e.target.value)}
                                         />
                                     </fieldset>
                                     <fieldset className="form-group">
@@ -78,17 +89,23 @@ export function Register() {
                                             className="form-control form-control-lg"
                                             type="text"
                                             value={firstName}
-                                            onChange={e => setFirstName(e.target.value)}
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                            pattern="[A-Za-z\s]*"
+                                            title="Imię nie może zawierać liczb ani znaków specjalnych"
+                                            required
                                         />
                                     </fieldset>
-                                        <fieldset className="form-group">
-                                            <input
-                                                placeholder="Nazwisko"
-                                                className="form-control form-control-lg"
-                                                type="text"
-                                                value={lastName}
-                                                onChange={e => setLastName(e.target.value)}
-                                            />
+                                    <fieldset className="form-group">
+                                        <input
+                                            placeholder="Nazwisko"
+                                            className="form-control form-control-lg"
+                                            type="text"
+                                            value={lastName}
+                                            onChange={(e) => setLastName(e.target.value)}
+                                            pattern="[A-Za-z\s]*"
+                                            title="Nazwisko nie może zawierać liczb ani znaków specjalnych"
+                                            required
+                                        />
                                     </fieldset>
                                     <fieldset className="form-group">
                                         <input
@@ -96,26 +113,29 @@ export function Register() {
                                             className="form-control form-control-lg"
                                             type="password"
                                             value={password}
-                                            onChange={e => setPassword(e.target.value)}
+                                            onChange={(e) => setPassword(e.target.value)}
                                         />
                                     </fieldset>
 
                                     <div className="d-flex justify-content-center align-items-center">
-                                        <button className="button-login btn btn-lg pull-xs-right" onClick={handleSubmit}>
+                                        <button
+                                            className="button-login btn btn-lg pull-xs-right"
+                                            onClick={handleSubmit}
+                                        >
                                             Zarejestruj się
                                         </button>
-
                                     </div>
                                     <div className="forgot-pass d-flex justify-content-center align-items-center">
                                         <a href="/logowanie"> Masz już konto?</a>
                                     </div>
-                                    <div className="login-failed d-flex justify-content-center align-items-center"><br />{ registerFailed}</div>
-                                    <div className="forgot-pass d-flex justify-content-center align-items-center">
+                                    <div className="login-failed d-flex justify-content-center align-items-center">
+                                        <br />
+                                        {registerFailed}
                                     </div>
+                                    <div className="forgot-pass d-flex justify-content-center align-items-center"></div>
                                 </fieldset>
                             </form>
                         </div>
-
                     </div>
                 </div>
             </div>
