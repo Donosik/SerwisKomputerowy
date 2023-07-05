16b8de45346ded2484f212a954e888fc72041ac2
Damian <damian1003@onet.pl>
2023-07-05 17:19:58 +0200

    Dodany endpoint w parts

--- Backend/Services/Interfaces/IPartService.cs
+++ Backend/Services/Interfaces/IPartService.cs
@@ -6,6 +6,7 @@ public interface IPartService
 {
     IEnumerable<Part> GetParts();
     Part GetPart(int id);
+    IEnumerable<Part> PartsSearchedByName(String name, bool isUsed);
     bool CreatePart(Part part);
     bool EditPart(Part part);
     bool DeletePart(int id);
